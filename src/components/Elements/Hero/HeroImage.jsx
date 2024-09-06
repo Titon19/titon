b670db178c9083fb856bbaa0d1b97be365dc3ee1
Titon Meisya Kresna <104065865+Titon19@users.noreply.github.com>
2024-09-07 02:46:26 +0700

    Update HeroImage.jsx

--- src/components/Elements/Hero/HeroImage.jsx
+++ src/components/Elements/Hero/HeroImage.jsx
@@ -3,7 +3,7 @@ const ImageHero = () => {
     <div className="image w-52 h-52 sm:w-80 sm:h-80 md:w-92 md:h-92 lg:w-96 lg:h-96">
       <img
         className="rounded-full grayscale hover:grayscale-0"
-        src="..\assets\images\heroImage.png"
+        src={require("src\assets\images\heroImage.png")}
         alt="heroImage"
       />
     </div>
